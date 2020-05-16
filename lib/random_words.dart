b35045c3b40e9898fd24526b25894dc619bbbe66
Raja Asyraf <rmasyraf@gmail.com>
2020-05-16 17:27:19 +0800

    Apply Format Document to properly format the code

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -12,29 +12,27 @@ class RandomWordsState extends State<RandomWords> {
 
   Widget _buildList() {
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, item) {
-        if(item.isOdd) return Divider();
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
 
-        final index = item ~/ 2;
-
-        if(index >= _randomWordPairs.length) {
-          _randomWordPairs.addAll(generateWordPairs().take(10));
-        }
-
-        return _buildRow(_randomWordPairs[index]);
-      });
+          final index = item ~/ 2;
+          if (index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_randomWordPairs[index]);
+        });
   }
 
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _savedWordPairs.contains(pair);
 
     return ListTile(
-      title: Text(pair.asPascalCase, 
-        style: TextStyle(fontSize: 18.0)
+      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
-      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border, 
-      color: alreadySaved ? Colors.red : null,),
       onTap: () {
         setState(() {
           if (alreadySaved) {
@@ -48,27 +46,23 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
-            return ListTile(title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)));
-          });
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)));
+      });
 
-          final List<Widget> divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles
-          ).toList();
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
 
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved WordPairs'),
-            ),
-            body: ListView(children: divided),
-          );
-        }
-      )
-    );
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved WordPairs'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 
   Widget build(BuildContext context) {
